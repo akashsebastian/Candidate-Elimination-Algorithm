553dde724bf8f6f5ea13c15d3ddc26a33db762a5
akashsebastian <akash.sebastian6@gmail.com>
2017-09-21 18:24:19 +0530

    Training data parsed

--- candidate_elimination.cpp
+++ candidate_elimination.cpp
@@ -1,12 +1,16 @@
 #include<iostream>
 #include<vector>
 #include<map>
+#include<string>
+#include<fstream>
+#include<set>
+#include<stdlib.h>
 using namespace std;
 
 int main(){
     int num_of_attributes;
     //cout<<"Enter the number of attributes: ";
-    cin>>num_of_attributes;
+    /*cin>>num_of_attributes;
     vector <int> number_of_values(num_of_attributes+1);
     vector <vector<int> > values_of_attributes(num_of_attributes+1);
     for(int i=0;i<num_of_attributes;i++){
@@ -19,6 +23,38 @@ int main(){
     values_of_attributes[num_of_attributes].resize(number_of_values[num_of_attributes]);
     for(int i=0;i<number_of_values[num_of_attributes];i++)
         cin>>values_of_attributes[num_of_attributes][i];
+    set <vector<int> > specific_boundary,general_boundary;
+    vector<int> temp_vec(num_of_attributes),current_train(num_of_attributes+1);
+    for(int i=0;i<num_of_attributes;i++)
+        temp_vec[i]=-1;
+    specific_boundary.insert(temp_vec);
+    for(int i=0;i<num_of_attributes;i++)
+        temp_vec[i]=INT_MAX;
+    general_boundary.insert(temp_vec);*/
+    ifstream in("zoo.data.txt");
+    string str;
+    while(getline(in,str)){
+        int length=str.size(),first=0,offset=0,counter=0;
+        string temp_str;
+        for(int i=0;i<length;i++){
+            if(str[i]==','||i==length-1){
+                if(!first){
+                    first=1;
+                    offset=i+1;
+                    continue;
+                }
+                if(i==length-1)
+                    temp_str = str.substr(offset,i-offset+1);
+                else
+                    temp_str = str.substr(offset,i-offset);
+                int temp_int = atoi(temp_str.c_str());
+                current_train[counter]=temp_int;
+                counter++;
+                offset=i+1;
+            }
+        }
+
+    }
     /*for(int i=0;i<=num_of_attributes;i++){
         for(int j=0;j<number_of_values[i];j++)
             cout<<values_of_attributes[i][j];
